ae61a0c9d476a20a40ff322ad63c0a9f86419062
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2024-09-28 01:41:28 +0900

    feat: :sparkles: Add mainmenu.rs
    Issue: #1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use bevy::prelude::*;
 
-const GAMETITLE: &str = "Timing Game";
-const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
+mod mainmenu;
+
+use crate::mainmenu::{
+    mainmenu_setup,
+    mainmenu_update,
+};
+
+pub const GAMETITLE: &str = "Timing Game";
+pub const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
 const BG_COLOR: Color = Color::srgb(0.25, 0.25, 0.25);
+
+pub const BG_IMAGE_PATH: &str = "images/mainmenu.png";
+pub const FONT_MEDIUM_PATH: &str = "fonts/FiraMono-Medium.ttf";
+pub const FONT_BOLD_PATH: &str = "fonts/FiraSans-Bold.ttf";
 
 #[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
 pub enum AppState {
@@ -29,6 +40,8 @@ fn main() {
         .insert_resource(ClearColor(BG_COLOR))
         .insert_resource(Time::<Fixed>::from_seconds(1.0 / 60.0))
         .add_systems(Startup, setup_camera)
+        .add_systems(OnEnter(AppState::Mainmenu), mainmenu_setup)
+        .add_systems(Update, mainmenu_update.run_if(in_state(AppState::Mainmenu)))
         .run();
 }
 
