a4963fa4d6d922c4483cc64706a422ab267203e6
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2024-09-27 22:55:56 +0900

    feat: :heavy_plus_sign: Add Bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use bevy::prelude::*;
+
+const GAMETITLE: &str = "Timing Game";
+const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
+const BG_COLOR: Color = Color::srgb(0.25, 0.25, 0.25);
+
+#[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum AppState {
+    #[default]
+    Mainmenu,
+    Ingame,
+    Gameover
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    resolution: WINDOW_SIZE.into(),
+                    title: GAMETITLE.to_string(),
+                    ..default()
+                }),
+                ..default()
+            })
+            .set(ImagePlugin::default_nearest())
+        )
+        .init_state::<AppState>()
+        .insert_resource(ClearColor(BG_COLOR))
+        .insert_resource(Time::<Fixed>::from_seconds(1.0 / 60.0))
+        .add_systems(Startup, setup_camera)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
